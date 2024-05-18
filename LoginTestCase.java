a9473684c7f397703d3453705441233075c0704f
Muhammad Mahad Rasool <77567883+mahadrasool@users.noreply.github.com>
2024-05-18 22:55:03 +0500

    Adding test cases

--- LoginTestCase.java
+++ LoginTestCase.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+// Test Class for E-commerce app
 public class LoginTestCase {
 
     public static void main(String[] args) {
