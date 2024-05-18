acd57404cd8baf3367460ce5258fb07277b47eee
Muhammad Mahad Rasool <77567883+mahadrasool@users.noreply.github.com>
2024-05-18 22:54:14 +0500

    Update AddToCart.java

--- AddToCart.java
+++ AddToCart.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
- 
+// AddToCart Class for E-commerce app
 public class AddToCart {
 
     public static void main(String[] args) {
